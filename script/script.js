6c658cb14e426417c765d00328bb47835064a0e7
Ross Nelson <RossN430@gmail.com>
2019-11-29 02:28:47 -0600

    Added nav bar

---
+++ script/script.js
@@ -0,0 +1,9 @@
+/* script.js */
+/* Ross Nelson */
+
+document.getElementById("nav").innerHTML =
+    "<ul>" +
+    "<li><a href='https://www.linkedin.com/in/RossNel'>LinkedIn</a></li>" +
+    "<li> <a href = 'https://www.github.com/Rossmallow' > GitHub </a></li>" +
+    "<li><a href = 'mailto: RossN430@gmail.com'> Email </a></li>" +
+    "</ul>";
