30f5af6e2be21b2a8ecfc8903fb90ea161c0ac59
Ross Nelson <RossN430@gmail.com>
2019-11-29 12:51:02 -0600

    Replaced text in the NavBar with icons

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 
 document.getElementById("nav").innerHTML =
     "<ul>" +
-    "<li><a href='https://www.linkedin.com/in/RossNel'>LinkedIn</a></li>" +
-    "<li> <a href = 'https://www.github.com/Rossmallow' > GitHub </a></li>" +
-    "<li><a href = 'mailto: RossN430@gmail.com'> Email </a></li>" +
+    "<li><a href='https://www.linkedin.com/in/RossNel'><img src='images/linkedin.png' alt='LinkedIn'></a></li>" +
+    "<li> <a href='https://www.github.com/Rossmallow'><img src='images/github.png' alt='GitHub'></a></li>" +
+    "<li><a href='mailto: RossN430@gmail.com'><img src='images/mail.png' alt='Email'></a></li>" +
     "</ul>";
