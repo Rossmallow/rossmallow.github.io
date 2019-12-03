d487c718081e1a9eab98c495e2d04dd1494f1990
Ross Nelson <RossN430@gmail.com>
2019-12-03 01:07:46 -0600

    Update tech page

--- tech/scripts/script.js
+++ tech/scripts/script.js
@@ -1,10 +1,9 @@
-/* scripts/script.js */
+/* tech/scripts/script.js */
 /* Ross Nelson */
 
 /* Nav Bar */
 document.getElementById("techInternal").innerHTML =
     "<ul>" +
-    "<li><a target='_parent' href='../index.html' tabindex='1'>Home</a></li>" +
-    "<li><a target='_parent' href='projects/index.html' tabindex='2'>Projects</a></li>" +
-    "<li><a target='_parent' href='contact/index.html' tabindex='3'>Contact</a></li>" +
+    "<li><a target='_parent' href='index.html' tabindex='1'>Home</a></li>" +
+    "<li><a target='_parent' href='https://github.com/Rossmallow?tab=repositories' tabindex='2'>Projects</a></li>" +
     "</ul>";
